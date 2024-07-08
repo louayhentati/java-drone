0c5f91894ba077dd1e97e5fcb14354f65ea5f084
louayhentati <Louay.hh7@gmail.com>
2024-07-08 12:21:27 +0200

    Update DroneDynamicsApp.java

--- java/src/gui/DroneDynamicsApp.java
+++ java/src/gui/DroneDynamicsApp.java
@@ -6,10 +6,25 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import org.json.JSONObject;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+/**
+ * The DroneDynamicsApp class provides methods for interacting with an API to retrieve
+ * dynamic information about drones, such as their ID, battery status, and current dynamics.
+ * It also includes a nested DroneDynamics class that encapsulates drone dynamics attributes
+ * using JavaFX properties for easy integration with UI components.
+ * <p>
+ * The class includes methods for:
+ * -id: Retrieving the ID of a drone from a given drone type URL.
+ * -Battery: Calculating and returning the battery percentage based on current power and battery capacity.
+ * <p>
+ * The nested DroneDynamics class represents drone dynamics with attributes
+ * <p>
+ * External dependencies include an API wrapper (api) for making HTTP requests and handling JSON responses.
+ * The API endpoint and authentication details are specified within the initializeApi method.
+ */
 
 public class DroneDynamicsApp  {
+
     public static Integer id(String dronetypeUrl) throws IOException {
         // Extract the relevant part of the URL
         String[] urlParts = dronetypeUrl.split("/");
@@ -23,6 +38,39 @@ public class DroneDynamicsApp  {
         // Extract manufacturer and typename
         Integer id = responseObject.getInt("id");
         return id;
+    }
+    public static int Battery(String dronetypeUrl,int power) throws IOException {
+
+        String[] urlParts = dronetypeUrl.split("/");
+        String apiEndpoint = "/api/drones/" + urlParts[urlParts.length - 1]+"/";
+        // Initialize the API connection
+        api myApi4 = new api("/api/drones/", "http://dronesim.facets-labs.com", "Token 40a9557fac747f55c11ad20c85caac1d43654911", "Louay");
+        myApi4.createConnection(apiEndpoint);
+        String response4 = myApi4.retrieveResponse();
+        // Parse the JSON response
+        JSONObject responseObject = new JSONObject(response4);
+        // Extract manufacturer and typename
+        String dronetypeurl1 = responseObject.getString("dronetype");
+
+        String[] urlParts2 = dronetypeurl1.split("/");
+
+        String apiEndpoint2 = "/api/dronetypes/" + urlParts2[urlParts2.length - 1]+"/";
+        // Initialize the API connection
+        api myApi5 = new api("/api/dronetypes/", "http://dronesim.facets-labs.com", "Token 40a9557fac747f55c11ad20c85caac1d43654911", "Louay");
+        myApi5.createConnection(apiEndpoint2);
+        String response5 = myApi5.retrieveResponse();
+        // Parse the JSON response
+        JSONObject responseObject1 = new JSONObject(response5);
+
+        int battery_capacity = responseObject1.getInt("battery_capacity");
+
+        if (battery_capacity == 0) {
+            // Handle division by zero or invalid battery capacity appropriately
+            return 0;
+        }
+
+        float batteryPercentage = ((float) power * 100 / battery_capacity);
+        return Math.round(batteryPercentage);
     }
 
 
@@ -63,14 +111,7 @@ public class DroneDynamicsApp  {
 
         public String getTimestamp() {
 
-            // Parse the current timestamp string to LocalDateTime
-            LocalDateTime dateTime = LocalDateTime.parse(timestamp.get(), DateTimeFormatter.ISO_DATE_TIME);
-
-            // Define a date-time formatter with the desired format
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMMM-dd HH:mm:ss");
-
-            // Format the current date-time and return the formatted string
-            return dateTime.format(formatter);
+           return timestamp.get();
         }
 
         public int getSpeed() {
@@ -98,84 +139,7 @@ public class DroneDynamicsApp  {
         }
 
         public int getBatteryStatus() {
-            int maxBatteryCapacity;
-            switch (drone.get()) {
-                case 1:
-                    maxBatteryCapacity = 5100;
-                    break;
-                case 2:
-                    maxBatteryCapacity = 380;
-                    break;
-                case 3:
-                    maxBatteryCapacity = 7500;
-                    break;
-                case 4:
-                    maxBatteryCapacity = 5000;
-                    break;
-                case 5:
-                    maxBatteryCapacity = 5400;
-                    break;
-                case 6:
-                    maxBatteryCapacity = 5870;
-                    break;
-                case 7:
-                    maxBatteryCapacity = 5400;
-                    break;
-                case 8:
-                    maxBatteryCapacity =7100 ;
-                    break;
-                case 9:
-                    maxBatteryCapacity = 2700;
-                    break;
-                case 10:
-                    maxBatteryCapacity = 4280;
-                    break;
-                case 11:
-                    maxBatteryCapacity = 500;
-                    break;
-                case 12:
-                    maxBatteryCapacity = 100;
-                    break;
-                case 13:
-                    maxBatteryCapacity = 150;
-                    break;
-                case 14:
-                    maxBatteryCapacity = 500;
-                    break;
-                case 15:
-                    maxBatteryCapacity = 3500;
-                    break;
-                case 16:
-                    maxBatteryCapacity = 1100;
-                    break;
-                case 17:
-                    maxBatteryCapacity = 750;
-                    break;
-                case 18:
-                    maxBatteryCapacity = 550;
-                    break;
-                case 19:
-                    maxBatteryCapacity = 2800;
-                    break;
-                case 20:
-                    maxBatteryCapacity = 2500;
-                    break;
-                default:
-                    maxBatteryCapacity = 7100; // Handle unrecognized drone types
-                    break;
-            }
-
-            // Calculate battery percentage
-            float batteryPercentage = ((float) batteryStatus.get() / maxBatteryCapacity) * 100;
-            return Math.round(batteryPercentage);
-
-
-            /*
-            int currentBatteryStatus = batteryStatus.get();
-            int maxBatteryCapacity = droneType.getBatteryCapacity(); // Accessing battery capacity from DroneType instance
-            float batteryPercentage = ((float) currentBatteryStatus / maxBatteryCapacity) * 100;
-            return Math.round(batteryPercentage);*/
-           // return batteryStatus.get();
+            return batteryStatus.get();
         }
 
         public String getLastSeen() {
@@ -186,4 +150,5 @@ public class DroneDynamicsApp  {
             return status.get();
         }
     }
+
 }
